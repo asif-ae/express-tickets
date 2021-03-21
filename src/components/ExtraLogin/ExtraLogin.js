53a6d61d4f331e0d4d781311fcf492f8086d1f8b
Asif Estiak <me.asif.ae@gmail.com>
2021-03-21 13:40:22 +0600

    added duplicate codes function

--- src/components/ExtraLogin/ExtraLogin.js
+++ src/components/ExtraLogin/ExtraLogin.js
@@ -14,21 +14,39 @@ if (!firebase.apps.length) {
 }
 
 const ExtraLogin = () => {
+  // Use History Hook
   const history = useHistory();
+  // Use Location Hook
   const location = useLocation();
   const { from } = location.state || { from: { pathname: "/" } };
+
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  // Update Error Message
   const [errors, setErrors] = useState({
     error: ''
   });
+  // Destructuring UseState
   const {error} = errors;
 
   const handleGithubSignIn = () => {
     const githubProvider = new firebase.auth.GithubAuthProvider();
 
+    // Fetching data
+    firebaseAuth(githubProvider);
+  }
+  const handleTwitterSignIn = () => {
+    const twitterProvider = new firebase.auth.TwitterAuthProvider();
+
+    // Created a function for simplicity
+    firebaseAuth(twitterProvider);
+  }
+
+  // Firebase Authentication Function
+  const firebaseAuth = (providers) => {
     firebase
     .auth()
-    .signInWithPopup(githubProvider)
+    .signInWithPopup(providers)
     .then((result) => {
       const user = result.user;
       const {displayName} = user;
@@ -42,49 +60,35 @@ const ExtraLogin = () => {
       setErrors(newUserInfo);
     });
   }
-  const handleTwitterSignIn = () => {
-    const twitterProvider = new firebase.auth.TwitterAuthProvider();
 
-    firebase
-    .auth()
-    .signInWithPopup(twitterProvider)
-    .then((result) => {
-      const user = result.user;
-      const {displayName} = user;
-      const signedInUser = {name: displayName};
-      setLoggedInUser(signedInUser);
-      history.replace(from);
-    }).catch((error) => {
-      const errorMessage = error.message;
-      const newUserInfo = {...errors};
-      newUserInfo.error = errorMessage;
-      setErrors(newUserInfo);
-    });
+
+  // Display Authentication Button Function
+  const displayAuthBoxes = (box, handleSignIn, icon, fontIcon, text, brands) => {
+    return (
+      <div className="d-flex justify-content-center my-3">
+        <div className={`rounded-pill ${box}`} onClick={handleSignIn}>
+          <div className="p-2 d-flex align-items-center">
+            <div className={`d-inline-block rounded-circle icon-style ${icon}`}>
+              <FontAwesomeIcon icon={fontIcon}></FontAwesomeIcon>
+            </div>
+            <div className={`d-inline-block mx-3 px-3 text-size ${text}`}>Continue with {brands}</div>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   return (
     <div>
       <p className="text-center text-danger">{error}</p>
-      <div className="d-flex justify-content-center my-3">
-        <div className="rounded-pill twitter-box" onClick={handleTwitterSignIn}>
-          <div className="p-2 d-flex align-items-center">
-            <div className="d-inline-block rounded-circle icon-style twitter-icon">
-              <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
-            </div>
-            <div className="d-inline-block mx-3 px-3 text-size twitter-text">Continue with Twitter</div>
-          </div>
-        </div>
-      </div>
-      <div className="d-flex justify-content-center my-3">
-        <div className="rounded-pill github-box" onClick={handleGithubSignIn}>
-          <div className="p-2 d-flex align-items-center">
-            <div className="d-inline-block rounded-circle icon-style github-icon">
-              <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-            </div>
-            <div className="d-inline-block mx-3 px-3 text-size github-text">Continue with Github</div>
-          </div>
-        </div>
-      </div>
+      {/* For Twitter */}
+      {
+        displayAuthBoxes("twitter-box", handleTwitterSignIn, "twitter-icon", faTwitter, "twitter-text", "Twitter")
+      }
+      {/* For Github */}
+      {
+        displayAuthBoxes("github-box", handleGithubSignIn, "github-icon", faGithub, "github-text", "Github")
+      }
     </div>
   );
 };
